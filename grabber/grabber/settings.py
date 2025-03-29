f75889688cef8189f21352f1932c17ed81a5c6c1
Maksym Antipov <maxtipov@ukr.net>
2025-03-29 10:41:27 +0100

    moved the secret key SECRET_KEY to the .env file

--- grabber/grabber/settings.py
+++ grabber/grabber/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6g!&wql0lj3^3a@3lpv@(p^in^0jf2$%8ozpf8io-wg+!9*($%'
+SECRET_KEY = config("SECRET_KEY")
+DEBUG = config("DEBUG", default=False, cast=bool)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
