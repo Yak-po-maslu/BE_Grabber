9e01e08a7ee09561fcc0e7a4ad41ea7b1b07430e
Maksym Antipov <maxtipov@ukr.net>
2025-04-29 15:16:53 +0200

    Revert settings.py to match main

--- grabber/grabber/settings.py
+++ grabber/grabber/settings.py
@@ -125,18 +125,11 @@ WSGI_APPLICATION = 'grabber.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-#DATABASES = {
-   # 'default': dj_database_url.config(default=os.getenv("DB_EXTERNAL_URL"))
+    'default': dj_database_url.config(default=os.getenv("DB_EXTERNAL_URL"))
         #'ENGINE': 'django.db.backends.sqlite3',
        # 'NAME': BASE_DIR / 'db.sqlite3',
 
-#}
+}
 
 
 # Password validation
