0ef256c985f30e18f4da01cf156b36b1120d0781
Dmytrenko Kyrylo <kirilldm6703@gmail.com>
2025-05-02 19:26:26 +0300

    add user-roles (admin, seller, buyer, moderator).

--- grabber/users/admin.py
+++ grabber/users/admin.py
@@ -3,7 +3,7 @@ from .models import CustomUser
 
 @admin.register(CustomUser)
 class CustomUserAdmin(admin.ModelAdmin):
-    list_display = ('id', 'email', 'first_name', 'last_name', 'is_active', 'is_staff', 'phone_number', "date_joined", "location", "password")
+    list_display = ('id', 'email', 'first_name', 'last_name', 'is_active', 'is_staff', 'phone_number', "date_joined", "location", "password",'role')
     search_fields = ('email', 'first_name', 'last_name')
 
 # Register your models here.
