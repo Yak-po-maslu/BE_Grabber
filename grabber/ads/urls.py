3003520928f25e23062c7dcb8b49a142971c978e
Dmytrenko Kyrylo <kirilldm6703@gmail.com>
2025-05-11 13:50:51 +0300

    api ads/ = create ads

--- grabber/ads/urls.py
+++ grabber/ads/urls.py
@@ -4,7 +4,7 @@ approve_ad, reject_ad)
 
 urlpatterns = [
     path('', index.index, name='ads_index'),
-    path('create-ad/',create_ad.AsyncCreateAdsView.as_view(), name='create-ad'),
+    path('',create_ad.AsyncCreateAdsView.as_view(), name='create-ad'),
     path('<int:ad_id>/add-image/', add_image_to_ads.AddImageToAdsAPIView.as_view(), name='add-image-to-ads'),
     path("moderation/",get_moderation.GetModerationView.as_view(), name='get_moderation'),
     path('<int:ad_id>/approve/', approve_ad.ApproveAdAPIView.as_view(), name='approve-ad'),
