5de323d1f5fee7e78a597673a343649be46bf4a4
Maksym Antipov <maxtipov@ukr.net>
2025-05-16 10:42:32 +0200

    change sync_to_async

--- grabber/ads/views/get_main_page.py
+++ grabber/ads/views/get_main_page.py
@@ -1,16 +1,25 @@
-from rest_framework.generics import ListAPIView
+from asgiref.sync import sync_to_async
+from adrf.views import APIView as AsyncAPIView
+from rest_framework.response import Response
+from rest_framework import status
 from ads.models import Ad
 from ads.serializers.ad import AdSerializer
 from django.db.models import Q
 
-class MainPageAdListView(ListAPIView):
-    serializer_class = AdSerializer
+class MainPageAdListView(AsyncAPIView):
+    async def get(self, request):
+        # Асинхронно витягуємо базовий queryset
+        base_qs = await sync_to_async(lambda: Ad.objects.filter(status="approved"))()
 
-    def get_queryset(self):
-        queryset = Ad.objects.filter(status="approved")
-        search = self.request.query_params.get("search")
+        # Обробка пошуку
+        search = request.query_params.get("search")
         if search:
-            queryset = queryset.filter(
-                Q(title__icontains=search) | Q(description__icontains=search)
-            )
-        return queryset
+            base_qs = await sync_to_async(
+                lambda: base_qs.filter(
+                    Q(title__icontains=search) | Q(description__icontains=search)
+                )
+            )()
+
+        # Сериалізація
+        serializer = AdSerializer(base_qs, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
