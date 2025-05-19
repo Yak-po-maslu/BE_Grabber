2d87ea1b86f8580fb9d7e4d55c0ece763ea90b2c
Dmytrenko Kyrylo <kirilldm6703@gmail.com>
2025-05-19 21:26:04 +0300

    return if access expired or non exist:
    Error: response status is 401
    
    Response body
    Download
    {
      "detail": "Access token not provided",
      "code": "authentication_failed"
    }

--- grabber/users/authentication/authentication.py
+++ grabber/users/authentication/authentication.py
@@ -1,5 +1,5 @@
 from rest_framework_simplejwt.authentication import JWTAuthentication
-from rest_framework_simplejwt.exceptions import TokenError
+from rest_framework_simplejwt.exceptions import TokenError, AuthenticationFailed
 from django.urls import reverse
 
 
@@ -8,17 +8,18 @@ class JWTAuthFromCookie(JWTAuthentication):
 
         login_url = reverse("users:login")
         register_url = reverse("users:register")
+        refresh_url = reverse("users:refresh")
         # Пропускаем аутентификацию для пути логина и регистрации
-        if request.path in [login_url, register_url]:
+        if request.path in [login_url, register_url, refresh_url]:
             return None
 
         access_token = request.COOKIES.get("access_token")
         if access_token is None:
-            return None
+            raise AuthenticationFailed("Access token expired or not provided")
 
         try:
             validated_token = self.get_validated_token(access_token)
         except TokenError:
-            return None  # Возвращаем None, если токен невалиден
+            return AuthenticationFailed(detail="Access token expired")
 
         return self.get_user(validated_token), validated_token
