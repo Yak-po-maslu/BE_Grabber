b7528db0d377d779498655787456d40c5997d02d
Maksym Antipov <maxtipov@ukr.net>
2025-05-21 23:38:37 +0200

    added admin register class_AdAdmin

--- grabber/ads/admin.py
+++ grabber/ads/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
+from ads.models import Ad, UploadedImageV1
 
-from ads.models import UploadedImageV1
+
+@admin.register(Ad)
+class AdAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'price', 'status', 'user', 'created_at']
+    list_filter = ['status', 'category', 'created_at']
+    search_fields = ['title', 'description', 'category']
 
 
-# Register your models here.
 @admin.register(UploadedImageV1)
 class UploadedImageAdmin(admin.ModelAdmin):
     model = UploadedImageV1
