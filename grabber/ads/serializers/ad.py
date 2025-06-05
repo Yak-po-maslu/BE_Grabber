9a4e66fab8b42225a083a5455f3454858ae0054a
Maksym Antipov <maxtipov@ukr.net>
2025-06-05 11:48:31 +0200

    added info for seller (name, phone_number)

--- grabber/ads/serializers/ad.py
+++ grabber/ads/serializers/ad.py
@@ -1,8 +1,16 @@
 from rest_framework import serializers
 from ..models import Ad
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
 
+
+class SellerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['first_name', 'phone_number']
 class AdSerializer(serializers.ModelSerializer):
+    seller = SellerSerializer(source='user', read_only=True)
     class Meta:
         model = Ad
         fields = "__all__"
