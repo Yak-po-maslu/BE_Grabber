aaca9db439d59f6d9a70eb94b0eac9dbd695fe21
Dmytrenko Kyrylo Oleksandrovich <Dmytrenko.K.O@nmu.one>
2025-06-10 22:25:54 +0300

    GET /ads/:id, get /ads/ Повертає: title, description, price, category list of image URLs user info (тільки ім’я/нікнейм) дата створення, перегляди, location. Також оптимізація при get/ads для цього кейсу (введення select_related у 8 разів зменшило час обробки запиту). Також змінений патч профіль. Після додавання show_phone з явився баг відсутності можливості зміни номеру телефону

--- grabber/ads/serializers/ad.py
+++ grabber/ads/serializers/ad.py
@@ -3,17 +3,26 @@ from ..models import Ad
 
 
 class AdSerializer(serializers.ModelSerializer):
+    location = serializers.CharField(source='user.location')
+    first_name = serializers.CharField(source='user.first_name')
+    phone_number = serializers.SerializerMethodField()
 
-    category_name = serializers.SerializerMethodField()
-    category_id = serializers.SerializerMethodField()
+    category_name = serializers.CharField(source='category.name', read_only=True)
+    category_id = serializers.IntegerField(source='category.id', read_only=True)
 
+
+    #
+    #phone_number = models.CharField(max_length=255, blank=False, default='+38033333333')
+    #show_phone = models.BooleanField(default=False)
+    #
     class Meta:
         model = Ad
         fields = ['id', 'description', 'price', 'user_id',
-                  'title', 'status', 'category_name', 'category_id', 'images']
+                  'title', 'status', 'category_name', 'category_id', 'images', 'created_at','phone_number',
+                  'first_name', 'location']
 
-    def get_category_name(self, obj):
-        return obj.category.name if obj.category else None
 
-    def get_category_id(self, obj):
-        return obj.category.id if obj.category else None
+
+    def get_phone_number(self, obj):
+        return obj.user.phone_number if obj.user.show_phone else 'user is not show phone'
+
