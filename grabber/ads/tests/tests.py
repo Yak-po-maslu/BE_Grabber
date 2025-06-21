8663f0239341098dfce55bb5109b1ca3e65957c4
Maksym Antipov <maxtipov@ukr.net>
2025-06-21 10:41:20 +0200

    Added unit test for recommended ads endpoint and reorganized test files by feature for better structure.

--- grabber/ads/tests/tests.py
+++ grabber/ads/tests/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
-from .models import Ad
+from ..models import Ad
 from django.core.exceptions import ValidationError
 
 User = get_user_model()
