69dd1e2d70d444458928a117d9a78c4a6cbcdc35
Maksym Antipov <maxtipov@ukr.net>
2025-06-21 10:39:35 +0200

    Moved RecommendedAdsAPIView to a separate file (views/get_recommended_ads.py)

--- grabber/ads/views/get_ads.py
+++ grabber/ads/views/get_ads.py
@@ -4,8 +4,6 @@ from ads.models import Ad
 from ads.serializers.ad import AdSerializer
 from ads.filters.filters import AdFilter
 from ads.configs.ad_view_config import AdViewConfig  # імпортуємо нову конфігурацію
-from rest_framework.views import APIView
-from rest_framework.response import Response
 class AdViewSet(viewsets.ModelViewSet):  # <-- замінили ReadOnlyModelViewSet
     serializer_class = AdSerializer
 
@@ -17,13 +15,3 @@ class AdViewSet(viewsets.ModelViewSet):  # <-- замінили ReadOnlyModelVie
     filterset_class = AdViewConfig.filterset_class
     ordering_fields = AdViewConfig.ordering_fields
     search_fields = AdViewConfig.search_fields
-
-class RecommendedAdsAPIView(APIView):
-    def get(self, request):
-        ads = Ad.objects.filter(
-            is_recommended=True,
-            status='approved'
-        ).only('id', 'title', 'price', 'images', 'user_id', 'created_at', 'category')[:20]
-
-        serializer = AdSerializer(ads, many=True)
-        return Response(serializer.data)
